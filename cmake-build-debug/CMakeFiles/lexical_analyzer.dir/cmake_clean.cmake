021567d02d82896aa405f42b9f2b1efbaf17ae4c
kiloveice <50243152+kiloveice@users.noreply.github.com>
2020-04-22 19:57:43 +0800

    0.1 all update
    工程文件全部上传

---
+++ cmake-build-debug/CMakeFiles/lexical_analyzer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lexical_analyzer.dir/main.cpp.obj"
+  "lexical_analyzer.exe"
+  "lexical_analyzer.exe.manifest"
+  "lexical_analyzer.pdb"
+  "liblexical_analyzer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lexical_analyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
